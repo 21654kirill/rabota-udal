fd38156032f0cbe9c23ebab5998cf59437f5bde6
21654 <21654@1k129s12.dune.edu>
2024-04-02 17:02:48 +0200

    доп файл коммит

---
+++ src/laba4/fail.java
@@ -0,0 +1,11 @@
+
+package laba4;
+
+
+public class fail {
+    public static void run() {
+        // TODO code application logic here
+        System.out.println("У меня все отлично");
+       
+    }
+}
