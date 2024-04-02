44f011d1f24509f3f4d468f0264446d08039261a
21654 <21654@1k129s12.dune.edu>
2024-04-02 16:54:54 +0200

    Исходный коммит

---
+++ src/laba4/Laba4.java
@@ -0,0 +1,10 @@
+package laba4;
+
+public class Laba4 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Здравствуйте");
+    }
+    
+}
