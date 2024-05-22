012cf2bd56f74510ec7f1a1b5008307466201646
21654kirill <kirill.misevich@mail.ru>
2024-05-22 07:38:29 +0200

    Add files via upload

---
+++ Laba4.java
@@ -0,0 +1,14 @@
+package laba4;
+
+public class Laba4 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+          System.out.println("А затем каникулы");
+
+        System.out.println("Скоро обед");
+  
+    }
+    
+}
